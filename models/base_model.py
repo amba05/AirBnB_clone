d48fa396a0c6618c968b5b2c1935c8d20ad08888
amba05 <emma.amba@gmail.com>
2022-08-07 19:04:39 +0000

    Refix console

--- models/base_model.py
+++ models/base_model.py
@@ -4,8 +4,17 @@ import uuid
 from datetime import datetime
 import models
 
+
 class BaseModel:
+    """Base class for all models"""
+
     def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel.
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+
         if (len(kwargs) == 0):
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -14,30 +23,32 @@ class BaseModel:
 
         else:
             format_data = "%Y-%m-%dT%H:%M:%S.%f"
-            kwargs["created_at"] = datetime.strptime(kwargs["created_at"], format_data)
-            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"], format_data)
+            kwargs["created_at"] = datetime.strptime(
+                    kwargs["created_at"], format_data)
+
+            kwargs["updated_at"] = datetime.strptime(
+                    kwargs["updated_at"], format_data)
 
             for key, val in kwargs.items():
                 if "__class__" not in key:
                     setattr(self, key, val)
 
     def __str__(self):
-        val = '[' + self.__class__.__name__ + "] (" + self.id + ") " + str(self.__dict__ )
-        return val
+        """Representation of BaseModel instances"""
+
+        clsName = self.__class__.__name__
+        return "[{}] ({}) {}".format(clsName, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        val = self.__dict__
-        val["__class__"] = self.__class__.__name__
 
-        for key in val:
-            if key == 'created_at':
-                val[key] = str(val[key])
+        format_data = "%Y-%m-%dT%H:%M:%S.%f"
+        dct_cp = dict(self.__dict__)
+        dct_cp['__class__'] = self.__class__.__name__
+        dct_cp['updated_at'] = self.updated_at.strftime(format_data)
+        dct_cp['created_at'] = self.created_at.strftime(format_data)
 
-            if key == 'updated_at':
-                val[key] = str(val[key])
-
-        return val
+        return (dct_cp)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,11 +5,18 @@
 
 import json
 import models
+from models.base_model import BaseModel
+
 
 class FileStorage:
     '''
         Serializes instances to JSON file and deserializes to JSON file.
+
+        Attributes:
+            __file_path (str): The name of the file to save objects to.
+            __objects (dict): A dictionary of instantiated objects.
     '''
+
     __file_path = "file.json"
     __objects = {}
 
@@ -17,52 +24,53 @@ class FileStorage:
         '''
             Return the dictionary
         '''
-        return self.__objects
+        return type(self).__objects
 
     def new(self, obj):
         '''
             Set in __objects the obj with key <obj class name>.id
-
-            (Basically this function gets the cls name of the passed obj 
+            (Basically this function gets the cls name of the passed obj
             and its id and uses it to store the obj instance dictionary in
             the private class attribute '__objects')
-            
+
             Args:
-                obj : An instance object.
+                obj (dict) : An instance object.
         '''
         key = str(obj.__class__.__name__) + "." + str(obj.id)
-        FileStorage.__objects[key] = obj
+        type(self).__objects[key] = obj
 
-    
     def save(self):
         """
         Write the JSON serialization of a list of objects to a file.
-        
+
         Args:
             list_objs (list): A list of inherited Base instances.
         """
-        
-        filename = FileStorage.__file_path
 
         obj_dict = {}
 
-        for key, value in FileStorage.__objects.items():
-            obj_dict[key] = value
+        for key, val in type(self).__objects.items():
+            obj_dict[key] = val.to_dict()
 
-        with open(filename, mode="w", encoding="UTF-8") as jsonfile:
-            json.dump(str(obj_dict), jsonfile)
-
+        with open(type(self).__file_path, "w", encoding="UTF-8") as jsonfile:
+            json.dump(obj_dict, jsonfile)
 
     def reload(self):
-
-        filename = FileStorage.__file_path
+        filename = type(self).__file_path
         try:
-            with open(filename, mode="r", encoding="UTF8") as jsonfile:
-                FileStorage.__objects = json.load(jsonfile)
+            with open(filename, mode="r", encoding="UTF-8") as jsonfile:
+                type(self).__objects = json.load(jsonfile)
 
-            for key, val in FileStorage.__objects.items():
-                class_name = val["__class__"]
-                FileStorage.__objects[key] = class_name(**val)
+                for key, val in type(self).__objects.items():
+                    # Explanation - get the classname from each
+                    # entry to the json file
+                    # then use it to get the obj class e.g BaseModel
+                    # from the classes list
+                    # in model __init__ file and initialize the
+                    # object with the **kwargs
+                    class_name = val['__class__']
+                    del val["__class__"]
+                    type(self).__objects[key] = eval(class_name)(**val)
 
         except FileNotFoundError:
             pass

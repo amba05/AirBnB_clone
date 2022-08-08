d53d339f0b1a857e049a4fa494a125c91fa2836e
Amba <80679383+amba05@users.noreply.github.com>
2022-08-08 02:18:48 +0100

    Update console.py

--- console.py
+++ console.py
@@ -5,12 +5,6 @@
 import cmd
 from models import storage
 from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.city import City
-from models.amenity import Amenity
-from models.state import State
-from models.review import Review
 import shlex
 
 
